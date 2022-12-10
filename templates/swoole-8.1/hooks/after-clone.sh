eac8e7c0437635a83e25ebe1d9b0f70f56909f57
Ivan Koryukov <koryukov@greensight.ru>
2022-12-10 12:11:04 +0300

    после клонирования создатся .env.testing

--- templates/swoole-8.1/hooks/after-clone.sh
+++ templates/swoole-8.1/hooks/after-clone.sh
@@ -12,3 +12,20 @@ elc compose run --rm -u$(id -u):$(id -g) --entrypoint="" app composer install
 echo -e "\e[34mPreconfigure\e[0m"
 cp .env.example .env
 elc compose run --rm -u$(id -u):$(id -g) --entrypoint="" app php artisan key:generate
+
+cp .env .env.testing
+
+sed -E 's/(DB_DATABASE=.*)/\1_test/' -i .env.testing
+sed -E 's/(CACHE_DRIVER=).*/CACHE_DRIVER=array/' -i .env.testing
+sed -E 's/(QUEUE_CONNECTION=).*/QUEUE_CONNECTION=sync/' -i .env.testing
+
+sed -E 's/(MAIL_HOST=).*/\1/' -i .env.testing
+sed -E 's/(ELASTICSEARCH_HOST=).*/\1/' -i .env.testing
+sed -E 's/(.*_SERVICE_HOST=).*/\1/' -i .env.testing
+sed -E 's/(.*_DOMAIN=).*/\1/' -i .env.testing
+sed -E 's/(.*_BROKER_LIST=).*/\1/' -i .env.testing
+sed -E 's/(.*_URL=).*/\1/' -i .env.testing
+
+elc restart
+elc php artisan migrate
+elc php artisan migrate --env=testing
