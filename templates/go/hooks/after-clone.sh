78e75a9580d798d2c593317d05044ecbc3ea0e48
Ivan Koryukov <koryukov@greensight.ru>
2026-07-21 18:44:31 +0300

    EF-1161 add go template and customer-api-gateway-go

---
+++ templates/go/hooks/after-clone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd "$SVC_PATH"
+
+echo -e "\e[34mSet git hooks\e[0m"
+elc set-hooks .git_hooks
+
+echo -e "\e[34mInstall dependencies\e[0m"
+elc run go install github.com/air-verse/air@latest
+elc run go mod tidy
+
+echo -e "\e[34mPreconfigure\e[0m"
+cp .env.example .env
+elc run /var/www/artisan key:generate
+
+elc restart
